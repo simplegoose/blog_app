090c87940c2f05857c160a60b97935ab17949d00
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-09 22:37:58 +0300

    Add generated schema to working directory

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_01_09_193009) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.string "text"
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -25,8 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_09_193009) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
@@ -38,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_09_193009) do
     t.string "title"
     t.integer "comments_counter"
     t.integer "likes_counter"
-    t.bigint "author_id", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
