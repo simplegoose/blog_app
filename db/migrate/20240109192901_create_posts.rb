ca1c728bfe7916bd5c11472b89168bf6352b719d
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-09 23:08:41 +0300

    fix required changes

--- db/migrate/20240109192901_create_posts.rb
+++ db/migrate/20240109192901_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :text
+      t.text :text
       t.string :title
       t.integer :comments_counter
       t.integer :likes_counter
