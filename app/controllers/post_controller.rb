41fc64636d9e8bae5aa50b396b18d1a9313d9127
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-15 20:21:20 +0300

    Create controller for posts

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,11 @@
+class PostController < ApplicationController
+  # Get users/:user_id/posts
+  def index
+    @posts = Post.where(author_id: params[:user_id])
+  end
+
+  #Get users/:user_id/posts/:id
+  def show
+    @post = Post.where(author_id: params[:user_id], id: params[:id])
+  end
+end
