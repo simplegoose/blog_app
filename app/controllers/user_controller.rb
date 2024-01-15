853f4139753c1b21984f52d0426ee002b1f8aa52
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-15 20:21:33 +0300

    Create controller for users

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,11 @@
+class UserController < ApplicationController
+  #Get /users/
+  def index
+    @users = User.all
+  end
+
+  #Get /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+end
