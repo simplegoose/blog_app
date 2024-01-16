4b33db060df63d57a5e06934b581d6e3cfc7a5cc
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-16 19:48:01 +0300

    Make changes to post controller, fetch user in place of post Fetch user and posts in show controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
   # Get users/:user_id/posts
   def index
-    @posts = Post.where(author_id: params[:user_id])
+    @user = User.find(params[:user_id])
   end
 
   # Get users/:user_id/posts/:id
   def show
-    @post = Post.where(author_id: params[:user_id], id: params[:id])
+    @user = User.find(params[:user_id])
+    @post = Post.where(author_id: params[:user_id], id: params[:id])[0]
   end
 end
