25dcf9888aaabb045de332d041c3af2b350fb3d7
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-17 19:59:20 +0300

    Fethc all posts when user navigates to index page Fetch all comments when user navigates to show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,13 @@ class PostsController < ApplicationController
   # Get users/:user_id/posts
   def index
     @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
   end
 
   # Get users/:user_id/posts/:id
   def show
     @user = User.find(params[:user_id])
     @post = Post.where(author_id: params[:user_id], id: params[:id])[0]
+    @comments = Comment.where(user: params[:user_id])
   end
 end
