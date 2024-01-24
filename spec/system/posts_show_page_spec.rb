4ebdd351c8a0a96dec5dab17c52f2ca2731f436a
Timothy Njoroge <timothy.njoroge@jithvar.com>
2024-01-24 18:22:41 +0300

    Replace file contents with the correct ones

--- spec/system/posts_show_page_spec.rb
+++ spec/system/posts_show_page_spec.rb
@@ -1,93 +1,77 @@
 require 'rails_helper'
 
-RSpec.describe 'Post/Index', type: :system do
+RSpec.describe 'Post/Show', type: :system do
   let(:user) do
     User.create(name: 'Harry', photo: 'https://photos.com/harrystyles', bio: 'harry Styles here!')
   end
 
+  let(:user2) do
+    User.create(name: 'Niall', photo: 'https://photos.com/niall', bio: 'Niall here!')
+  end
+
   let(:post) do
-    Post.create(author: user, text: 'text', title: 'First!')
-  end
-
-  let(:post2) do
-    Post.create(author: user, text: 'text', title: 'Second!')
-  end
-
-  let(:post3) do
-    Post.create(author: user, text: 'text', title: 'Third!')
+    Post.create(author: user, text: 'text', title: 'First!', likes_counter: 1)
   end
 
   let(:like) do
-    Like.create(user:, post:)
+    Like.create(user: user, post: post)
   end
 
   let(:comment) do
-    Comment.create(user:, post:, text: 'text')
+    Comment.create(user: user, post: post, text: 'text')
+  end
+
+  let(:comment2) do
+    Comment.create(user: user2, post: post, text: 'text')
   end
 
   before :each do
     user.save
+    user2.save
     post.save
-    post2.save
-    post3.save
     like.save
     comment.save
+    comment2.save
   end
-
-  describe 'Index page' do
-    it 'I can see the user\'s profile pic.' do
-      visit user_posts_path(user)
-      expect(page).to have_css("img[src*='#{user.photo}']")
-    end
-
-    it 'I can see the user\'s profile name.' do
-      visit user_posts_path(user)
-      expect(page).to have_content(user.name)
-    end
-
-    it 'I can see the user\'s post count.' do
-      visit user_posts_path(user)
-      expect(page).to have_content(user.post_counter)
-    end
+  
+  describe 'Show page' do
 
     it 'I can see a post title.' do
-      visit user_posts_path(user)
+      visit user_post_path(user, post)
       expect(page).to have_content(post.title)
     end
 
-    it 'I can see some of post body.' do
-      visit user_posts_path(user)
-      expect(page).to have_content(post.text)
-      expect(page).to have_content(post2.text)
-      expect(page).to have_content(post3.text)
-    end
-
-    it 'I can see a post comment.' do
-      visit user_posts_path(user)
-      expect(page).to have_content(comment.text)
+    it 'I can see who wrote the post.' do
+      visit user_post_path(user, post)
+      expect(page).to have_content(post.author.name)
     end
 
     it 'I can see how many comments a post has.' do
-      visit user_posts_path(user)
+      visit user_post_path(user, post)
       expect(page).to have_content(post.comments_counter)
     end
 
     it 'I can see how many likes a post has.' do
-      visit user_posts_path(user)
+      visit user_post_path(user, post)
       expect(page).to have_content(post.likes_counter)
     end
 
-    it 'I can see a section for pagination if there are more posts than fit on the view.' do
-      visit user_posts_path(user)
-      expect(page).to have_content('Pagination')
+    it 'I can see a post body.' do
+      visit user_post_path(user, post)
+      expect(page).to have_content(post.text)
     end
 
-    it 'I can visit post show page.' do
-      visit user_posts_path(user)
-
-      click_on post.title.to_s
-
-      expect(page).to have_content(post.title)
+    it 'I can see username of each commenter.' do
+      visit user_post_path(user, post)
+      expect(page).to have_content(comment.user.name)
+      expect(page).to have_content(comment2.user.name)
     end
+
+    it 'I can see comment of each user.' do
+      visit user_post_path(user, post)
+      expect(page).to have_content(comment.text)
+      expect(page).to have_content(comment2.text)
+    end
+
   end
 end
